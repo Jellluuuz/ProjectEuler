10c15f4a72333dae21af2de14a4267c7376e2019
Jelle <Jellespitz@hotmail.com>
2020-10-27 14:10:58 +0100

    solve 57

--- PEprob57.py
+++ PEprob57.py
@@ -1,26 +1,28 @@
 import time
-from fractions import Fraction
 
-t = time.time()
+t0 = time.time()
 
-deci_fracs = []
+key_val = [1]
+denominator = [2]
+numerator = [3]
 
-a = Fraction(1./2)
-for i in range(1000):
-    deci_fracs.append(str(1 + a))
-    a = 1./(2+a)
-    a = Fraction(str(1./(2+a))).limit_denominator()
-    print a
+
+for i in range(999):
+    key_val.append(denominator[-1])
+    denominator.append(2*denominator[-1] + key_val[-2])
+
+    numerator.append(key_val[-1] + denominator[-1])
+
+print key_val
+print numerator
+print denominator
 
 count = 0
-for i in deci_fracs:
-    num = i.split('/')
-    #print num
-    if len(num[0]) > len(num[1]):
+
+for i in range(len(numerator)):
+    if len(str(numerator[i])) > len(str(denominator[i])):
         count += 1
-    print num[0], num[1]
+
 
 print count
-
-print time.time() - t
-
+print time.time() - t0
